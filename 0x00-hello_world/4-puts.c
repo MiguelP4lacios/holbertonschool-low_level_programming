164d054e4ec9f7a0bfea015e9de7fc9973cc721b
Miguel <oswaldo9605@gmail.com>
2020-02-05 19:38:25 +0000

    Update "

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
-/*How to use puts for display a String */
+
+
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual");
 	return (0);
-}
+}  /*How to use puts for display a String */

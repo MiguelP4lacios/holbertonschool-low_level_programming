bfafbcfc98edb2ba468ea348d83c129db5ec317c
Miguel <oswaldo9605@gmail.com>
2020-02-06 21:46:05 +0000

    ASCII 2

---
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdio.h>
+/**
+  * main - Entry point
+  *
+  * Return: Always 0 (Success)
+  */
+int main(void)
+{
+	int i;
+	int j;
+
+	for (i = 48 ; i <= 57 ; i++)
+	{
+		for (j = 48 ; j <=57 ; j++)
+		{
+			putchar (i);
+			putchar (j);
+			if (i != 57 || j != 57)
+			{
+				putchar (44);
+				putchar (32);
+			}
+		}
+	}
+
+	putchar('\n');
+	return (0);
+}

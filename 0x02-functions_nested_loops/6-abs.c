70e65fb4974523a0156b9bb94485e18b357211de
Miguel <oswaldo9605@gmail.com>
2020-02-10 23:52:01 +0000

    3th advance

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+/**
+  * _abs - say me the absolute value of an integer.
+  *
+  * Return: value n
+  */
+int _abs(int n)
+{
+	if(n > 0)
+	{
+		return (n);
+	}
+	else if (n < 0)
+	{
+		return (n*-1);
+	}
+	else
+	{
+		return (0);
+	}
+
+}

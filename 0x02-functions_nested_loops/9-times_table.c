54e0914a67a77bc85ebce303bf7b593a01c9a9bd
Miguel <oswaldo9605@gmail.com>
2020-02-11 19:47:16 +0000

    update table 9 betty

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,20 +7,20 @@
 void times_table(void)
 {
 	int i, j, k;
-	
+
 	for (i = 0 ; i < 10 ; i++)
 	{
 		for (j = 0 ; j < 10 ; j++)
 		{
 			k = i * j;
-			if ( k < 10)
+			if (k < 10)
 			{
-				if ( j != 0)
+				if (j != 0)
 				{
 					_putchar(' ');
 					_putchar(' ');
 				}
-				_putchar( k + '0');
+				_putchar(k + '0');
 			}
 			else
 			{
@@ -28,7 +28,7 @@ void times_table(void)
 				_putchar(k / 10 + '0');
 				_putchar(k % 10 + '0');
 			}
-			if ( j != 9)
+			if (j != 9)
 			{
 				_putchar(',');
 			}

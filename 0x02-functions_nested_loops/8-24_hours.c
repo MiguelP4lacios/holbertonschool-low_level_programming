b82c69039d54f8f53c1a296fabc0d375eacc860e
Miguel <oswaldo9605@gmail.com>
2020-02-11 19:00:17 +0000

    update3

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -10,8 +10,12 @@ void jack_bauer(void)
 
 	for (i = '0'; i <= '2'; i++)
 	{
-		for (j = '0'; j <= '3' ; j++)
+		for (j = '0'; j <= '9' ; j++)
 		{
+			if (i == '2' && j == '4')
+			{
+				break;
+			}
 			for (k = '0' ; k <= '5'; k++)
 			{
 				for (l = '0' ; l <= '9' ; l++)

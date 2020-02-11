c86706da513ba2be5ac0db67ce98aaff64d65826
Miguel <oswaldo9605@gmail.com>
2020-02-11 19:43:27 +0000

    update table 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -6,50 +6,36 @@
  */
 void times_table(void)
 {
-	int table[10] = {0,0,0,0,0,0,0,0,0,0} ;
-	int i; 
-	int r;
-
-	for (i = '0' ; i < '9' ; i++)
-	{
-		_putchar(table[0] + '0');
-		_putchar(',');
-		_putchar(' ');
-	}
-	_putchar('\n');
-	for (i = '0' ; i < '9'; i++)
-	{
-		
-		_putchar(table[0] + '0');
-		_putchar(',');
-		_putchar(' ');
-		_putchar(table[1] += 1);
-		_putchar(',');
-		_putchar(' ');
-		_putchar(table[2] += 2);
-		_putchar(',');
-		_putchar(' ');
-		_putchar(table[3] += 3);
-		_putchar(',');
-		_putchar(' ');
-		_putchar(table[4] += 4);
-		_putchar(',');
-		_putchar(' ');
-		_putchar(table[5] += 5);
-		_putchar(',');
-		_putchar(' ');
-		_putchar(table[6] += 6);
-		_putchar(',');
-		_putchar(' ');
-		_putchar(table[7] += 7 );
-		_putchar(',');
-		_putchar(' ');
-		_putchar(table[8] += 8);
-		_putchar(',');
-		_putchar(' ');
-		_putchar(table[9] += 9);
-		_putchar('\n');
-	}
+	int i, j, k;
 	
-
+	for (i = 0 ; i < 10 ; i++)
+	{
+		for (j = 0 ; j < 10 ; j++)
+		{
+			k = i * j;
+			if ( k < 10)
+			{
+				if ( j != 0)
+				{
+					_putchar(' ');
+					_putchar(' ');
+				}
+				_putchar( k + '0');
+			}
+			else
+			{
+				_putchar(' ');
+				_putchar(k / 10 + '0');
+				_putchar(k % 10 + '0');
+			}
+			if ( j != 9)
+			{
+				_putchar(',');
+			}
+			else
+			{
+				_putchar('\n');
+			}
+		}
+	}
 }

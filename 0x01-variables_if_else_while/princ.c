cf39e36a2d8541151c81809431dbfc667deb1aba
Miguel <oswaldo9605@gmail.com>
2020-02-12 17:51:17 +0000

    ALL

---
+++ 0x01-variables_if_else_while/princ.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdio.h>
+/**
+    * main - Entry point
+      *
+  * Return: Alway
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 'z' ; n > 'a'; n--)
+		putchar (n);
+	putchar ('\n');
+				
+	return (0);
+}

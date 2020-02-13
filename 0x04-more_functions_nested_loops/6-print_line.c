9558affdb81a4a6bbd99e761334c823553dc1a7a
Miguel Palacios <oswaldo9605@gmail.com>
2020-02-13 12:56:38 -0500

    6 betty

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,19 +1,19 @@
 #include "holberton.h"
 /**
   * print_line - Print n lines..
-  *
+  * @n: numbers line
   * Return: nothing
   */
 void print_line(int n)
 {
 	int i;
+
 	if (n <= 0)
 	{
 		_putchar('\n');
 	}
 	else
 	{
-
 		for (i = 1; i <= n; i++)
 		{
 			_putchar(95);

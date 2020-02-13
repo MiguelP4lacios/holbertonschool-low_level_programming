0b2c30e04252ade8ef91812eeb73a7c609ab1a74
Miguel Palacios <oswaldo9605@gmail.com>
2020-02-13 15:38:31 -0500

    fix

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -5,14 +5,14 @@
   * Return: nothing
   */
 void print_triangle(int size)
-{	
+{
 	int i, j, m;
 
 	if (size <= 0)
 	{
 		_putchar('\n');
 	}
-	else 
+	else
 	{
 		for (i = 1; i <= size; i++)
 		{
--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -24,7 +24,7 @@ int main(void)
 		}
 		else
 		{
-			printf ("%d ", i);
+			printf("%d ", i);
 		}
 	}
 	printf("\n");

11c849927ef8752e18b4c986394039dbc39d2a6e
Miguel Palacios <oswaldo9605@gmail.com>
2020-02-18 15:20:19 -0500

    update half v3

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -15,14 +15,14 @@ void puts_half(char *str)
 	}
 	if (len % 2 == 0)
 	{
-		for (i = len / 2; i <= len; i++)
+		for (i = len / 2; i < len; i++)
 		{
 			_putchar(str[i]);
 		}
 	}
 	else
 	{
-		for (i = ((len - 1) / 2) + 1; i <= len; i++)
+		for (i = ((len - 1) / 2) + 1; i < len; i++)
 		{
 			_putchar(str[i]);
 		}

ad5f46815194b00577cd2970dc9eaf27d6194390
Miguel Palacios <oswaldo9605@gmail.com>
2020-02-26 13:40:58 -0500

    fix betty, and output code

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -6,10 +6,15 @@
   */
 void _puts_recursion(char *s)
 {
-	_putchar(*s);
-	s++;
 	if (*s != '\0')
+	{
+		_putchar(*s);
+		s++;
 		_puts_recursion(s);
+	}
 	if (*s == '\0')
+	{
 		_putchar('\n');
+		return;
+	}
 }
--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -2,12 +2,12 @@
 /**
   * _strlen_recursion - returns the length of a string.
   * @s: string
-  * Return: length;;
+  * Return:length;
   */
 int _strlen_recursion(char *s)
 {
 	if (*s == '\0')
 		return (0);
 	else
-		return ( 1 + _strlen_recursion(s + 1));
+		return (1 + _strlen_recursion(s + 1));
 }

0d1663190f6677afaa3bebd708bbca906b90754a
Miguel Palacios <oswaldo9605@gmail.com>
2020-03-03 17:57:04 -0500

    argstostr ; )

---
+++ 0x0B-malloc_free/5-argstostr.c
@@ -0,0 +1,39 @@
+#include <stdlib.h>
+/**
+  * argstostr - concatenates all the arguments of your program
+  * @argc: number argument
+  * @argv: pointer of argument
+  * Return: p or NULL
+  */
+char *argstostr(int ac, char **av)
+{
+	int i, j, len, p_len;
+	char *p;
+	
+	len = 0;
+	p_len = 0;
+	
+	if (ac == 0 || av == 0)
+		return (NULL);
+	for (i = 0; i < ac ; i++)
+	{
+		for (j = 0; *(*(av + i) + j) != '\0'; j++)
+			len++;
+	}
+	p = malloc(sizeof(char) * (len + 1));
+	if (p == NULL)
+		return (NULL);
+	for (i = 0; i < ac ; i++)
+	{
+		for (j = 0; *(*(av + i) + j) != '\0'; j++)
+		{
+			p[p_len] = *(*(av + i) + j);
+			p_len++;
+		}
+		p[p_len] = '\n';
+		p_len++;
+	}
+	p[p_len] = '\0';
+	return (p);
+}
+

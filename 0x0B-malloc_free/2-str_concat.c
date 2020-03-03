606819f35680363570b748fd0e24c180b9a9c0e8
Miguel Palacios <oswaldo9605@gmail.com>
2020-03-03 15:01:35 -0500

    fix output check v2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -21,14 +21,18 @@ int str_len(char *s)
 char *str_concat(char *s1, char *s2)
 {
 	char *A;
-	int i, j;
-	int t = str_len(s1) + str_len(s2);
+	int i = 0;
+	int j;
+	int t = str_len(s1) + str_len(s2) + 1;
 
 	A = malloc(sizeof(char) * t);
 	if (A == NULL)
 		return (NULL);
-	for (i = 0; s1 && s1[i] != '\0'; i++)
+	for (j = 0; s1 && s1[j] != '\0'; j++)
+	{
 		A[i] = s1[i];
+		i++;
+	}
 	for (j = 0; s2 && s2[j] != '\0'; j++)
 	{
 		A[i] = s2[j];

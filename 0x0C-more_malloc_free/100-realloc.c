4199f6f241220226fa388661dae846e3e5f61e2d
Miguel Palacios <oswaldo9605@gmail.com>
2020-03-05 17:18:17 -0500

    update bett

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -3,7 +3,7 @@
   * _realloc - reallocates a memory block using malloc and free.
   * @ptr: string
   * @old_size:is the size, in bytes, of the allocated space for ptr
-  * @new_size:is the new size, in bytes of the new memory block 
+  * @new_size:is the new size, in bytes of the new memory block
   * Return: ()
   */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)

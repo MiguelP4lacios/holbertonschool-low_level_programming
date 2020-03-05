493cac13f6d8ff24af4400e3bc7a4877a33108a3
Miguel Palacios <oswaldo9605@gmail.com>
2020-03-05 15:36:33 -0500

    fix betty

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,7 +1,7 @@
 #include <stdlib.h>
 int str_len(char *s);
 /**
-  * string_concat - concatenates two strings.
+  * string_nconcat - concatenates two strings.
   * @s1: string 1
   * @s2: string 2
   * @n: n for string 2

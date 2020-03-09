bd42dbc1c7da241a6741a2c9c830118e5731fe86
Miguel Palacios <oswaldo9605@gmail.com>
2020-03-09 13:57:37 -0500

    check

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef DOGH
+#define DOGH
+/**
+  * struct dog - Data group of a dog
+  * @name: Dog name
+  * @age: Dog age
+  * @owner: Dog owner
+  */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif /*DOGH*/

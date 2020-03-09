de8eda225b7c2b028f9cc5cd952f444935208f37
Miguel Palacios <oswaldo9605@gmail.com>
2020-03-09 18:01:31 -0500

    suelten los perros

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,16 @@
+#include "dog.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+  * free_dog - frees dogs.
+  * @d: pointer to struct dog
+  * Return: Nothing
+  */
+void free_dog(dog_t *d)
+{
+	if (d == NULL)
+		return;
+	free((*d).name);
+	free((*d).owner);
+	free(d);
+}

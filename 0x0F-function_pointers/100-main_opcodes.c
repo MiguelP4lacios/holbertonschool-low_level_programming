631c086461daacc174ef2436c42ea269499fa486
Miguel Palacios <oswaldo9605@gmail.com>
2020-03-11 19:01:06 -0500

    100-main_opcodes.c-----opcode

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "3-calc.h"
+/**
+  *main - prints the opcodes of its own main function.
+  *@argc: number argument
+  *@argv: pointer of argument
+  *Return: 0
+  */
+int main(int argc, char **argv)
+{
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+	if ( atoi(argv[1]) < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+}

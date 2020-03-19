4ae7b277e52ee7855001c1fd00bfbe8a54090103
Miguel Palacios <oswaldo9605@gmail.com>
2020-03-19 14:20:12 -0500

    tab 8 Betty style

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,50 +1,50 @@
 #include "3-calc.h"
 /**
-  *op_add - add two numbers
-  *@a: number 1
-  *@b: number 2
-  *Return: sum a and b
-  */
+	*op_add - add two numbers
+	*@a: number 1
+	*@b: number 2
+	*Return: sum a and b
+	*/
 int op_add(int a, int b)
 {
 	return (a + b);
 }
 /**
-  *op_sub - difference two numbers
-  *@a: number 1
-  *@b: number 2
-  *Return: sub a and b
-  */
+	*op_sub - difference two numbers
+	*@a: number 1
+	*@b: number 2
+	*Return: sub a and b
+	*/
 int op_sub(int a, int b)
 {
 	return (a - b);
 }
 /**
-  *op_mul - product two numbers
-  *@a: number 1
-  *@b: number 2
-  *Return: mul a and b
-  */
+	*op_mul - product two numbers
+	*@a: number 1
+	*@b: number 2
+	*Return: mul a and b
+	*/
 int op_mul(int a, int b)
 {
 	return (a * b);
 }
 /**
-  *op_div - divide a by b
-  *@a: number 1
-  *@b: number 2
-  *Return: div a by b
-  */
+	*op_div - divide a by b
+	*@a: number 1
+	*@b: number 2
+	*Return: div a by b
+	*/
 int op_div(int a, int b)
 {
 	return (a / b);
 }
 /**
-  *op_mod - remainder of the division of a by b
-  *@a: number 1
-  *@b: number 2
-  *Return:  mod a by b
-  */
+	*op_mod - remainder of the division of a by b
+	*@a: number 1
+	*@b: number 2
+	*Return:  mod a by b
+	*/
 int op_mod(int a, int b)
 {
 	return (a % b);

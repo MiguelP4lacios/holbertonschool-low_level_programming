12de050ebd8af2b430923af3b01090b1924da3ff
Miguel Palacios <oswaldo9605@gmail.com>
2020-03-27 16:31:12 -0500

    fix 1

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -1,11 +1,12 @@
 #include "holberton.h"
 /**
- * get_endianness - 
+ * get_endianness - function that checks the endianness.
  *
  * Return: 0 for big endian, 1 for little endian.
  */
 int get_endianness(void)
 {
 	unsigned int var;
+
 	return ((int) (((char *)&var)[0]));
 }

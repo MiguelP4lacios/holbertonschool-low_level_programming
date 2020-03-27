71608fba55fd357f53d4c9250f48579977a37f2c
Miguel Palacios <oswaldo9605@gmail.com>
2020-03-26 19:32:13 -0500

    betty

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -9,7 +9,7 @@ void print_binary(unsigned long int n)
 {
 	unsigned long int pos_exp = 1;
 
-	for (;pos_exp < n; pos_exp <<= 1)
+	for (; pos_exp < n; pos_exp <<= 1)
 	{
 		if (pos_exp > pos_exp << 1)
 			break;

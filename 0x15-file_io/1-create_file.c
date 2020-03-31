5d4e0784bc2bebf0164cb5a4c588b5f9b2237ffe
Miguel Palacios <oswaldo9605@gmail.com>
2020-03-31 18:37:22 -0500

    Fix conditional

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -18,7 +18,7 @@ int create_file(const char *filename, char *text_content)
 		text_content = "";
 
 	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC | O_EXCL, 0600);
-	if (fd == -1)
+	if (fd < 0)
 	{
 		if (errno == EEXIST)
 		{

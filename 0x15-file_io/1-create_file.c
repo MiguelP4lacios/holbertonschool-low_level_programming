63664443150926e79d1f758246d9d4ef3d2512b3
Miguel Palacios <oswaldo9605@gmail.com>
2020-03-31 18:42:30 -0500

    Fix close (fd)

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -16,7 +16,7 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	if (text_content == NULL)
 		text_content = "";
-	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC | O_EXCL, 0600);
 	if (fd < 0)
 	{
 		if (errno == EEXIST)
@@ -33,6 +33,7 @@ int create_file(const char *filename, char *text_content)
 	f_write = write(fd, text_content, str_len(text_content));
 	if (f_write == -1)
 		return (-1);
+	close(fd);
 	return (1);
 }
 /**

a4bb3e674f033ceec9466c67e8923e60b9751c1d
Miguel Palacios <oswaldo9605@gmail.com>
2020-03-31 18:40:21 -0500

    Update open

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -16,8 +16,7 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	if (text_content == NULL)
 		text_content = "";
-
-	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC | O_EXCL, 0600);
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	if (fd < 0)
 	{
 		if (errno == EEXIST)

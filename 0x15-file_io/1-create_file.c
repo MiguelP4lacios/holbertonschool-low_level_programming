173e3746d960469c5a128a2606116e1cef55f065
Miguel Palacios <oswaldo9605@gmail.com>
2020-03-31 18:29:36 -0500

    Fix code EXIST

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -17,9 +17,21 @@ int create_file(const char *filename, char *text_content)
 	if (text_content == NULL)
 		text_content = "";
 
-	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC | O_EXCL, 0600);
 	if (fd == -1)
-		return (-1);
+	{
+		printf ("enrro");
+		if (errno == EEXIST)
+		{
+			fd = open(filename, O_WRONLY | O_TRUNC);
+			if (fd == -1)
+				return (-1);
+		}
+		else
+		{
+			return (-1);
+		}
+	}
 	f_write = write(fd, text_content, str_len(text_content));
 	if (f_write == -1)
 		return (-1);
--- 0x15-file_io/holberton.h
+++ 0x15-file_io/holberton.h
@@ -4,6 +4,7 @@
 #include <stdlib.h>
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <sys/errno.h>
 #include <fcntl.h>
 #include <unistd.h>
 

c463b718d67048958d6880dd8688dd7aad4e6d46
Miguel Palacios <oswaldo9605@gmail.com>
2020-04-01 18:08:58 -0500

    Fix error QA review

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -14,32 +14,26 @@ int main(int ac, char *av[])
 	char buffer[1024];
 
 	if (ac != 3)
-	{
-		dprintf(STDERR_FILENO, "Usage: %s file_from file_to\n", av[0]);
-		exit(97);
-	}
+		dprintf(STDERR_FILENO, "Usage: %s file_from file_to\n", av[0]), exit(97);
 	if (av[1] == NULL)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
-		exit(98);
-	}
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]), exit(98);
 	fd1 = open(av[1], O_RDONLY);
 	if (fd1 == -1)
-		exit(98);
-	fd2 = open(av[2], O_CREAT | O_RDWR | O_TRUNC, 0664);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]), exit(98);
+	fd2 = open(av[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	if (fd2 == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
-		exit(99);
-	}
-	count = read(fd1, buffer, 1024);
-	count = write(fd2, buffer, count);
-	if (fd2 == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
-		exit(99);
-	}
-	close(fd1);
-	close(fd2);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]), exit(99);
+	do {
+		count = read(fd1, buffer, 1024);
+		if (fd1 == -1)
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]), exit(98);
+		count = write(fd2, buffer, count);
+		if (fd2 == -1)
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]), exit(99);
+	} while (count == 1024);
+	if (close(fd1) == -1)
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd1), exit(100);
+	if (close(fd2) == -1)
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd2), exit(100);
 	return (0);
 }

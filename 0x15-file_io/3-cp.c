610277310924dcb6d9e4d0bb12aba7cf0a3bf400
Miguel Palacios <oswaldo9605@gmail.com>
2020-04-01 18:45:55 -0500

    Fix conditional

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -15,8 +15,8 @@ int main(int ac, char *av[])
 
 	if (ac != 3)
 		dprintf(STDERR_FILENO, "Usage: %s file_from file_to\n", av[0]), exit(97);
-	if (av[1] == NULL)
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]), exit(98);
+	/*if (av[1] == NULL)
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]), exit(98);*/
 	fd1 = open(av[1], O_RDONLY);
 	if (fd1 == -1)
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]), exit(98);
@@ -25,10 +25,10 @@ int main(int ac, char *av[])
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]), exit(99);
 	do {
 		count = read(fd1, buffer, 1024);
-		if (fd1 == -1)
+		if (count == -1)
 			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]), exit(98);
 		count = write(fd2, buffer, count);
-		if (fd2 == -1)
+		if (count == -1)
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]), exit(99);
 	} while (count == 1024);
 	if (close(fd1) == -1)

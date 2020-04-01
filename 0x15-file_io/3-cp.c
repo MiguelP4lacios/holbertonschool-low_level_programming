9353df636f61048f5527deea7140373818828bad
Miguel Palacios <oswaldo9605@gmail.com>
2020-04-01 18:59:35 -0500

    fix 3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -10,25 +10,23 @@
 int main(int ac, char *av[])
 {
 	int fd1, fd2;
-	int count = 0;
+	int count = 0, count1 = 0;
 	char buffer[1024];
 
 	if (ac != 3)
 		dprintf(STDERR_FILENO, "Usage: %s file_from file_to\n", av[0]), exit(97);
-	/*if (av[1] == NULL)
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]), exit(98);*/
 	fd1 = open(av[1], O_RDONLY);
 	if (fd1 == -1)
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]), exit(98);
-	fd2 = open(av[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	fd2 = open(av[2], O_CREAT | O_WRONLY | O_TRUNC | O_EXCL, 0664);
 	if (fd2 == -1)
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]), exit(99);
 	do {
 		count = read(fd1, buffer, 1024);
 		if (count == -1)
 			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]), exit(98);
-		count = write(fd2, buffer, count);
-		if (count == -1)
+		count1 = write(fd2, buffer, count);
+		if (count1 == -1)
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]), exit(99);
 	} while (count == 1024);
 	if (close(fd1) == -1)

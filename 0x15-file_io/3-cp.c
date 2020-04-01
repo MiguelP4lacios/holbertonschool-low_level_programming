55ca5a6d01ce2b20c504fc31ab26f569ca4128bd
Miguel Palacios <oswaldo9605@gmail.com>
2020-03-31 21:19:00 -0500

    fix wr

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -26,7 +26,7 @@ int main(int ac, char **av)
 	fd1 = open(av[1], O_RDONLY);
 	if (fd1 == -1)
 		exit(98);
-	fd2 = open(av[2], O_CREAT | O_WRONLY | O_TRUNC | O_EXCL, 0664);
+	fd2 = open(av[2], O_CREAT | O_RDWR | O_TRUNC | O_EXCL, 0664);
 	if (fd2 == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
@@ -39,7 +39,12 @@ int main(int ac, char **av)
 		dprintf(STDOUT_FILENO, "Error: Can't write to %s\n", av[2]);
 		exit(99);
 	}
-	if (close(fd1) == -1 && close(fd2) == -1)
+	if (close(fd1) == -1)
+	{
+		dprintf(STDOUT_FILENO, "Error: Can't close fd FD_VALUE");
+		exit(100);
+	}
+	if (close(fd2) == -1)
 	{
 		dprintf(STDOUT_FILENO, "Error: Can't close fd FD_VALUE");
 		exit(100);

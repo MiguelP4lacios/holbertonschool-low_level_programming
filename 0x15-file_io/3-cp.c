3080236810099a99140ff723e2adb632b6d70456
Miguel Palacios <oswaldo9605@gmail.com>
2020-03-31 21:49:21 -0500

    Update 8

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -15,10 +15,10 @@ int main(int ac, char **av)
 
 	if (ac != 3)
 	{
-		dprintf(STDERR_FILENO, "Usage: %s file_from file_to\n", av[0]);
+		dprintf(STDERR_FILENO, "Usage: %s file_from file_to\n");
 		exit(97);
 	}
-	if (av == NULL)
+	if (av[1] == NULL)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
 		exit(98);

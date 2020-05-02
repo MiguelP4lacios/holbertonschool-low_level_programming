5cb5960dd4027c5e35abace558ea80c765bb75a1
Miguel Palacios <oswaldo9605@gmail.com>
2020-05-02 02:10:56 -0500

    Feat : free list

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * free_dlistint -
+ *
+ * Return: 
+ */
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *current;
+
+	while (head != NULL)
+	{
+		current = head;
+		head = head->next;
+		free(current);
+	}
+}

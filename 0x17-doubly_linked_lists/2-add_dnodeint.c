d4f7624f25913b85d2f614e3aee37e4247701717
Miguel Palacios <oswaldo9605@gmail.com>
2020-05-02 00:57:26 -0500

    Fix: return new node

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -19,12 +19,12 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	if (*head == NULL)
 	{
 		*head = new_node;
-		return (*head);
+		return (new_node);
 	}
 	(*head)->prev = new_node;
 	new_node->next = *head;
 	*head = new_node;
-	return (*head);
+	return (new_node);
 }
 /**
  * createNode - It creates a node whit data

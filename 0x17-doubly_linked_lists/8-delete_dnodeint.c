1da4ccaeaf3ebda2fdf6298ded680f81c134e96e
Miguel Palacios <oswaldo9605@gmail.com>
2020-05-02 22:47:01 -0500

    Feat: delete nodes

---
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,50 @@
+#include "lists.h"
+
+/**
+ * delete_dnodeint_at_index - function that deletes the node
+ * at index index of a dlistint_t linked list.
+ * @head: address of head main
+ * @index: position to delete
+ * Return: 1 if it succeeded, -1 if it failed
+ */
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+	dlistint_t *tmp = NULL, *corrent = *head;
+	size_t i = 0;
+
+	if (*head == NULL)
+	{
+		return (-1);
+	}
+	if (index == i)
+	{
+		tmp = *head;
+		*head = (*head)->next;
+		free(tmp);
+		return (1);
+	}
+	else
+	{
+		for (i = 0; corrent; i++)
+		{
+			if (index == i && corrent->next != NULL)
+			{
+				tmp = corrent;
+				corrent->prev->next = corrent->next;
+				corrent->next->prev = corrent->prev;
+				free(tmp);
+				return (1);
+			}
+			else if (index == i && corrent->next == NULL)
+			{
+				tmp = corrent;
+				corrent->prev->next = NULL;
+				free(tmp);
+				return (1);
+			}
+			corrent = corrent->next;
+		}
+
+	}
+	return (-1);
+}

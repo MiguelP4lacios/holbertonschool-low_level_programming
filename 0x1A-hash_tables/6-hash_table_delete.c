a265d6037d65cd2897a892327db226f5f94959b2
Miguel Palacios <oswaldo9605@gmail.com>
2020-05-29 15:46:02 -0500

    Feat: free

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,35 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_delete -
+ *
+ * Return: 
+ */
+void hash_table_delete(hash_table_t *ht)
+{
+	unsigned long int idx;
+	hash_node_t *head, *corrent;
+	hash_table_t *slots = ht;
+
+	if (slots == NULL)
+		return;
+	if (slots->array == NULL)
+	{
+		free(slots);
+		return;
+	}
+	for (idx = 0; idx < slots->size; idx++)
+	{
+		head = slots->array[idx];
+		while (head != NULL)
+		{
+			corrent = head;
+			head = head->next;
+			free(corrent->key);
+			free(corrent->value);
+			free(corrent);
+		}
+	}
+	free(slots->array);
+	free(slots);
+}

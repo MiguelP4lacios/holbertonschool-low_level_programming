92de7acfb7537c84fcec6c125685a3efcf480821
Miguel Palacios <oswaldo9605@gmail.com>
2020-05-29 13:39:53 -0500

    Fix logic and betty

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -1,31 +1,34 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_print -
+ * hash_table_print - print hash table like a dic
+ * @ht: hash table
  *
- * Return: 
+ * Return: nothing
  */
 void hash_table_print(const hash_table_t *ht)
 {
 	const hash_table_t *slots = ht;
 	hash_node_t *corrent;
-	unsigned long int idx;
+	unsigned long int idx, i = 0;
 
 	if (!slots)
 		return;
 	printf("{");
-	for ( idx = 0; idx < slots->size; idx++)
+	for (idx = 0; idx < slots->size; idx++)
 	{
 		corrent = slots->array[idx];
 
+		if ((i > 0) && (corrent))
+			printf(", ");
 		while (corrent)
 		{
 			printf("'%s': '%s'", corrent->key, corrent->value);
+			i++;
 			if (corrent->next)
 				printf(", ");
 			corrent = corrent->next;
 		}
-		
 	}
 	printf("}\n");
 }

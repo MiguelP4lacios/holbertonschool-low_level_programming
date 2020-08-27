74ccfb6c7042a95058444ae0c769b1e7bb76244e
Miguel Palacios <oswaldo9605@gmail.com>
2020-08-27 20:59:27 +0000

    [fix]: logic

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -16,9 +16,8 @@ int linear_search(int *array, size_t size, int value)
 
 	for (i = 0; i < size; i++)
 	{
-		if (array[i] != value)
-			printf("Value checked array[%lu] = %d\n", i, array[i]);
-		else
+		printf("Value checked array[%lu] = %d\n", i, array[i]);
+		if (array[i] == value)
 			return (i);
 	}
 	return (-1);
